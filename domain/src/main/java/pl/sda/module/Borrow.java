22b10f654d616034758b2a702df4e1e1a82db474
Marek Ciesla <ciesla.marek@gmail.com>
2018-07-22 09:30:46 +0200

    Added  Borrow class

--- domain/src/main/java/pl/sda/module/Borrow.java
+++ domain/src/main/java/pl/sda/module/Borrow.java
@@ -1,7 +1,20 @@
 package pl.sda.module;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
 /**
  * Created by MCK on 21.07.2018 12:42
  **/
+@Getter
+@Setter
 public class Borrow {
+
+    private Long borrowId;
+    private LocalDate borrowDate;
+    private Long borrowerId;
+    private Long bookId;
+
 }
