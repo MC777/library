8a0c6ef0689a1e1966517e31f7f8c0290611033b
Marek Ciesla <ciesla.marek@gmail.com>
2018-07-28 22:22:18 +0200

    Added Borrow Service added

--- service/src/main/java/pl/sda/service/BorrowService.java
+++ service/src/main/java/pl/sda/service/BorrowService.java
@@ -1,7 +1,20 @@
 package pl.sda.service;
 
+import pl.sda.module.Borrow;
+import pl.sda.repository.BorrowRepository;
+import pl.sda.repository.IBorrowRepository;
+
+import java.io.IOException;
+
 /**
  * Created by MCK on 21.07.2018 10:54
  **/
-public class BorrowService {
+public class BorrowService implements IBorrowService {
+
+    private IBorrowRepository borrowRepository = new BorrowRepository();
+
+    @Override
+    public void save(Borrow borrow) throws IOException {
+        borrowRepository.save(borrow);
+    }
 }
--- service/src/main/java/pl/sda/service/IBorrowService.java
+++ service/src/main/java/pl/sda/service/IBorrowService.java
@@ -1,7 +1,12 @@
 package pl.sda.service;
 
+import pl.sda.module.Borrow;
+
+import java.io.IOException;
+
 /**
  * Created by MCK on 21.07.2018 10:54
  **/
 public interface IBorrowService {
+    void save(Borrow borrow) throws IOException;
 }
